36b4bf89ad178ce9c2bccae61d9d65b6dad12fa3
Shu-Ting Huang <sthuang890702@gmail.com>
2022-01-02 03:35:04 -0500

    Modify my_server.py

--- my_server.py
+++ my_server.py
@@ -1,7 +1,10 @@
 import os
 import time
+import urllib.request
+
+external_ip = urllib.request.urlopen('https://api.ipify.org').read().decode('utf8')
 while True:
-    os.system("python3 manage.py runserver")
+    os.system("python3 manage.py runserver --insecure " + external_ip + ":80")
     for i in reversed(range(5)):
         print(i+1)
         time.sleep(1)
