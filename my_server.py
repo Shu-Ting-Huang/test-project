240f53a07f389c5a5286ff823f8df9a02736b70f
Shu-Ting Huang <sthuang890702@gmail.com>
2022-01-02 04:08:23 -0500

    Wait only 1 second for server to restart

--- my_server.py
+++ my_server.py
@@ -5,6 +5,6 @@ import urllib.request
 external_ip = urllib.request.urlopen('https://api.ipify.org').read().decode('utf8')
 while True:
     os.system("python3 manage.py runserver --insecure " + external_ip + ":80")
-    for i in reversed(range(5)):
+    for i in reversed(range(1)):
         print(i+1)
         time.sleep(1)
