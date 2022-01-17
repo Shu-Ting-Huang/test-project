9b7ac801999a7125cd798681edabd7139c49e6a8
Shu-Ting Huang <sthuang890702@gmail.com>
2022-01-16 21:20:32 -0500

    Show client IP on terminal

--- app/views.py
+++ app/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    print("Client IP is " + request.META['REMOTE_ADDR'])
     return render(request, 'index.html')
