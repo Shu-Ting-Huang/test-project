90434f171baad40214166979ef9080325a07fc2f
Shu-Ting Huang <sthuang890702@gmail.com>
2022-01-16 21:26:39 -0500

    Show client IP on web page

--- app/views.py
+++ app/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def index(request):
     print("Client IP is " + request.META['REMOTE_ADDR'])
-    return render(request, 'index.html')
+    return render(request, 'index.html', context={'client_IP': request.META['REMOTE_ADDR']})
