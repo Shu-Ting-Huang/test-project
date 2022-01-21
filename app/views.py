016a5e95a0dff58271e877e39b274f70bb718a75
Shu-Ting Huang <sthuang890702@gmail.com>
2022-01-20 21:12:58 -0500

    Allow the page to be embedded in frameset

--- app/views.py
+++ app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# Allow the page to be embedded in frameset
+from django.views.decorators.clickjacking import xframe_options_sameorigin
+@xframe_options_sameorigin
+
 def index(request):
     print("Client IP is " + request.META['REMOTE_ADDR'])
     return render(request, 'index.html', context={'client_IP': request.META['REMOTE_ADDR']})
